4dee62cd1d1574b29c3b05ed2ea311bd64c47f18
Julia-X-Luo <julia.luo98@gmail.com>
2017-03-04 13:39:34 -0500

    Update User.java

--- User.java
+++ User.java
@@ -6,13 +6,13 @@ public class User{
 	public String name;
 	public double balance; //owed-debt
 	public boolean isWinner;
-	public ArrayList<Transfer> paymentList;
+	public ArrayList<Transfer> transactions;
 	public int[] date; //date since last payment  
 	
 	public User(String name){
 		this.name = name;
 		this.isWinner = false;
-		this.paymentList = null;
+		this.transactions = null;
 		this.date = new int[3];
 		
 	}
@@ -20,8 +20,8 @@ public class User{
 	public void payment(User payee, Transfer t){ //"this" is paying "t" to "payee"
 		this.balance = this.balance - t.amount;
 		payee.balance = payee.balance + t.amount;
-		this.paymentList.add(t);
-		payee.paymentList.add(t);
+		this.transactions.add(t);
+		payee.transactions.add(t);
 		this.date = t.date;
 	}
 	
